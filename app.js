26a27a1583a389e541c359f030d6f3c37f243f2a
Matt Dailey <mjd@figma.com>
2018-02-15 10:55:48 -0800

    introduce express app that logs body

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(require('body-parser').json())
+
+app.get('/', (req, res) => {
+	console.log(req.body);
+	res.send('success');
+})
+
+app.post('/', (req, res) => {
+	console.log(req.body)
+	res.send('success')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
